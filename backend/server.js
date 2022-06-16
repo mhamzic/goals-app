daae9d37726939ebf1897dfb209eab19adcff4a0
mhamzic <mhamzic@gmail.com>
2022-06-16 13:43:29 +0200

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
+const path = require("path");
 
 connectDB();
 
@@ -15,6 +16,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+    app.get('/', (req,res)=>res.send('Please set to production mode'));
+}
 
 app.use(errorHandler);
 
